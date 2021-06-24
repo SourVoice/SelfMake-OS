900457f9557793a7ab0241643476c92b84408a03
SourVoice <2985122798@qq.com>
2021-06-24 16:32:16 +0800

    增加了os注释

--- day03/ipl与os连接/os.asm
+++ day03/ipl与os连接/os.asm
@@ -18,8 +18,8 @@
 ;TAB=4
         ORG     0xc200              ;磁盘内容装载到内存,磁盘0x4200处 + 0x8000得到操作系统内核位置,
                                     ;这里是指内存的位置
-        MOV     AL,0x13             ;BIOS调用显卡
-        MOV     AH,0x00
+        MOV     AL,0x13             ;BIOS调用显卡,选择0x13画面模式,画面模式下光标消失
+        MOV     AH,0x00             ;
         INT     0x10
     fin:
         hlt
