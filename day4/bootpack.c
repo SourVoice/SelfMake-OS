eea1db1607c789f2694da60e246ad03212d57487
SourVoice <2985122798@qq.com>
2021-06-26 10:00:02 +0800

    添加03作者版

---
+++ day4/bootpack.c
@@ -0,0 +1,14 @@
+void io_hit(void); /*函数调用,需要我们将nasm_func.asm和该c源文件连接*/
+void write_mem8(int addr, int data);
+
+void HariMain(void)
+{
+    int i;
+
+    for (i = 0xa0000; i < 0xaffff; i++)
+    {
+        write_mem8(i, 15); /*调用汇编_write_mem8([i],15)*/
+    }
+    for (;;)
+        io_hit(); /*执行汇编中的停机函数*/
+}
