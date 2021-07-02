95e3aad56f639c858460f81a0c0a4d5a140af67d
SourVoice <2985122798@qq.com>
2021-07-03 00:14:09 +0800

    显示变量值成功

---
+++ .VSCodeCounter/2021-07-02_23-38-15/results.md
@@ -0,0 +1,80 @@
+# Summary
+
+Date : 2021-07-02 23:38:15
+
+Directory d:\workstation\OS_SelfMake\Assembler
+
+Total : 256 files,  24225 codes, 752 comments, 2023 blanks, all 27000 lines
+
+[details](details.md)
+
+## Languages
+| language | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| assembly(DOS) | 19 | 13,460 | 142 | 52 | 13,654 |
+| nasm | 35 | 4,626 | 309 | 317 | 5,252 |
+| C++ | 120 | 4,200 | 200 | 1,304 | 5,704 |
+| C | 11 | 1,004 | 59 | 68 | 1,131 |
+| Makefile | 23 | 667 | 39 | 230 | 936 |
+| Batch | 45 | 225 | 0 | 41 | 266 |
+| Python | 2 | 41 | 3 | 10 | 54 |
+| Markdown | 1 | 2 | 0 | 1 | 3 |
+
+## Directories
+| path | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| . | 256 | 24,225 | 752 | 2,023 | 27,000 |
+| binary-os | 1 | 12,513 | 0 | 0 | 12,513 |
+| day00 | 1 | 22 | 0 | 0 | 22 |
+| day01 | 1 | 34 | 6 | 1 | 41 |
+| day02 | 1 | 51 | 7 | 4 | 62 |
+| day03 | 13 | 719 | 115 | 42 | 876 |
+| day03-break | 6 | 177 | 17 | 17 | 211 |
+| day03\32位模式下准备 | 2 | 127 | 15 | 5 | 147 |
+| day03\ipl与os连接 | 3 | 120 | 38 | 9 | 167 |
+| day03\导入C | 4 | 142 | 17 | 14 | 173 |
+| day04 | 71 | 3,419 | 214 | 571 | 4,204 |
+| day04\DrawSlaves | 9 | 519 | 43 | 64 | 626 |
+| day04\DrawTaskBar | 8 | 737 | 43 | 66 | 846 |
+| day04\DrawTrangle | 9 | 606 | 50 | 65 | 721 |
+| day04\add_protection | 8 | 441 | 28 | 33 | 502 |
+| day04\z_tools | 37 | 1,116 | 50 | 343 | 1,509 |
+| day04\z_tools\guigui00 | 11 | 661 | 28 | 173 | 862 |
+| day04\z_tools\haribote | 9 | 161 | 9 | 70 | 240 |
+| day04\z_tools\osa_qemu | 2 | 26 | 0 | 5 | 31 |
+| day04\z_tools\qemu | 2 | 6 | 0 | 1 | 7 |
+| day04\z_tools\qemu_9x | 1 | 2 | 0 | 1 | 3 |
+| day04\z_tools\win32 | 10 | 228 | 13 | 83 | 324 |
+| day05 | 76 | 5,009 | 290 | 691 | 5,990 |
+| day05\Rebuild-Struct | 8 | 761 | 43 | 68 | 872 |
+| day05\Rebuild-point-binfo | 8 | 758 | 43 | 68 | 869 |
+| day05\ShowCharacter | 8 | 901 | 48 | 67 | 1,016 |
+| day05\ShowMoreCharacter | 8 | 989 | 53 | 72 | 1,114 |
+| day05\ShowVariable | 7 | 484 | 53 | 73 | 610 |
+| day05\z_tools | 37 | 1,116 | 50 | 343 | 1,509 |
+| day05\z_tools\guigui00 | 11 | 661 | 28 | 173 | 862 |
+| day05\z_tools\haribote | 9 | 161 | 9 | 70 | 240 |
+| day05\z_tools\osa_qemu | 2 | 26 | 0 | 5 | 31 |
+| day05\z_tools\qemu | 2 | 6 | 0 | 1 | 7 |
+| day05\z_tools\qemu_9x | 1 | 2 | 0 | 1 | 3 |
+| day05\z_tools\win32 | 10 | 228 | 13 | 83 | 324 |
+| tolset | 48 | 1,163 | 53 | 353 | 1,569 |
+| tolset\z_new_o | 4 | 18 | 2 | 6 | 26 |
+| tolset\z_new_w | 4 | 10 | 1 | 3 | 14 |
+| tolset\z_osabin | 3 | 19 | 0 | 1 | 20 |
+| tolset\z_tools | 37 | 1,116 | 50 | 343 | 1,509 |
+| tolset\z_tools\guigui00 | 11 | 661 | 28 | 173 | 862 |
+| tolset\z_tools\haribote | 9 | 161 | 9 | 70 | 240 |
+| tolset\z_tools\osa_qemu | 2 | 26 | 0 | 5 | 31 |
+| tolset\z_tools\qemu | 2 | 6 | 0 | 1 | 7 |
+| tolset\z_tools\qemu_9x | 1 | 2 | 0 | 1 | 3 |
+| tolset\z_tools\win32 | 10 | 228 | 13 | 83 | 324 |
+| z_tools | 37 | 1,116 | 50 | 343 | 1,509 |
+| z_tools\guigui00 | 11 | 661 | 28 | 173 | 862 |
+| z_tools\haribote | 9 | 161 | 9 | 70 | 240 |
+| z_tools\osa_qemu | 2 | 26 | 0 | 5 | 31 |
+| z_tools\qemu | 2 | 6 | 0 | 1 | 7 |
+| z_tools\qemu_9x | 1 | 2 | 0 | 1 | 3 |
+| z_tools\win32 | 10 | 228 | 13 | 83 | 324 |
+
+[details](details.md)
---
+++ day05/ShowVariable/bootpack.c
@@ -0,0 +1,183 @@
+#include <stdio.h>
+
+#define COL8_000000 0
+#define COL8_ff0000 1
+#define COL8_00ff00 2
+#define COL8_ffff00 3
+#define COL8_0000ff 4
+#define COL8_ff00ff 5
+#define COL8_00ffff 6
+#define COL8_ffffff 7
+#define COL8_c6c6c6 8
+#define COL8_840000 9
+#define COL8_008400 10
+#define COL8_848400 11
+#define COL8_000084 12
+#define COL8_840084 13
+#define COL8_008484 14
+#define COL8_848484 15
+/**/
+void io_hlt(void);
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+void io_cli(void);
+void io_out8(int port, int data);
+/*填充屏幕像素*/
+void init_screen(unsigned char *vram, int xsize, int ysize);
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1); /*参数: vram地址,大小,色号,位置x,位置y,*/
+/* 严重怀疑这里使用结构体是为了节约空间的*/
+/*0x0ff0附近数据*/
+struct BOOTINFO
+{
+    char cyls, leds, vmode, reserve;
+    short scrnx, scrny;
+    char *vram;
+};
+//font部分
+static char font_A[16] = {
+    0x00, 0x18, 0x18, 0x18, 0x24, 0x24, 0x24,
+    0x24, 0x7e, 0x42, 0x42, 0xe7, 0x00, 0x00}; /*A字母的像素点阵*/
+void putfont8(char *vram, int xsize, char color, int x, int y, char *font);
+void putfont_asc(unsigned char *vram, int xsize, unsigned char color, int x, int y, unsigned char *str);
+void HariMain(void)
+{
+    init_palette(); /*调色板*/
+
+    char *vram;
+    char s[40];
+    int xsize, ysize;
+    struct BOOTINFO *binfo;
+    binfo = (struct BOOTINFO *)0x0ff0; /*指针binfo放入0x0ff0地址.*/
+    xsize = binfo->scrnx;
+    ysize = binfo->scrny;
+    vram = binfo->vram; /*这里注意,vram也是一个指针变量,指向0x0ff8*/
+
+    init_screen(vram, xsize, ysize); /*屏幕部分封装函数*/
+    putfont_asc(vram, xsize, COL8_840000, 20, 20, "what?");
+    putfont_asc(vram, xsize, COL8_840000, 40, 40, "what?");
+    putfont_asc(vram, xsize, COL8_840000, 60, 60, "what?");
+
+    sprintf(s, "scrnx = %d", xsize);                  /*新添内容,调用sprintf*/
+    putfont_asc(vram, xsize, COL8_00ffff, 16, 64, s); /*s用于记录字符串的地址*/
+    for (;;)
+        io_hlt(); /*执行汇编中的停机函数*/
+}
+void init_screen(unsigned char *vram, int xsize, int ysize)
+{
+    boxfill8(vram, xsize, COL8_ffff00, 0, 0, xsize - 1, ysize - 29); /*背景色*/
+
+    boxfill8(vram, xsize, COL8_ffffff, 3, ysize - 24, 59, ysize - 24);
+    boxfill8(vram, xsize, COL8_ffffff, 2, ysize - 24, 2, ysize - 4);
+    boxfill8(vram, xsize, COL8_848484, 3, ysize - 4, 59, ysize - 4);
+    boxfill8(vram, xsize, COL8_848484, 59, ysize - 23, 59, ysize - 5);
+    boxfill8(vram, xsize, COL8_000000, 2, ysize - 3, 59, ysize - 3);
+    boxfill8(vram, xsize, COL8_000000, 60, ysize - 24, 60, ysize - 3);
+
+    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize - 4, ysize - 24);
+    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize - 3);
+    boxfill8(vram, xsize, COL8_ffffff, xsize - 47, ysize - 3, xsize - 4, ysize - 3);
+    boxfill8(vram, xsize, COL8_ffffff, xsize - 3, ysize - 24, xsize - 3, ysize - 3);
+
+    putfont8(vram, xsize, COL8_00ffff, 2, 2, font_A);   /*文字在屏幕显示后再显示*/
+    putfont8(vram, xsize, COL8_00ffff, 12, 12, font_A); /*文字在屏幕显示后再显示*/
+    putfont8(vram, xsize, COL8_00ffff, 22, 22, font_A); /*文字在屏幕显示后再显示*/
+    putfont8(vram, xsize, COL8_00ffff, 32, 32, font_A); /*文字在屏幕显示后再显示*/
+}
+
+/*显示字符串*/
+void putfont_asc(unsigned char *vram, int xsize, unsigned char color, int x, int y, unsigned char *str)
+{
+    extern char hankaku[4096];
+    for (; *str != 0x00; str++)
+    {
+        putfont8(vram, xsize, color, x, y, hankaku + (*str) * 16);
+        x += 8;
+    }
+    return;
+}
+/*显示字体*/
+void putfont8(char *vram, int xsize, char color, int x, int y, char *font)
+{
+    int i;
+    char d;
+    char *p;
+    for (i = 0; i < 16; i++) /*16行*/
+    {
+        p = vram + (y + i) * xsize + x; /*切到指定行*/
+        d = font[i];
+        if ((d & 0x80) != 0)
+            *(p + 0) = color; /*和static font对应的话,将该位置填色*/
+        if ((d & 0x40) != 0)
+            *(p + 1) = color;
+        if ((d & 0x20) != 0)
+            *(p + 2) = color;
+        if ((d & 0x10) != 0)
+            *(p + 3) = color;
+        if ((d & 0x08) != 0)
+            *(p + 4) = color;
+        if ((d & 0x04) != 0)
+            *(p + 5) = color;
+        if ((d & 0x02) != 0)
+            *(p + 6) = color;
+        if ((d & 0x01) != 0)
+            *(p + 7) = color;
+    }
+    return;
+}
+/*初始化屏幕,填充颜色*/
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
+{
+    int x, y;
+    for (y = y0; y <= y1; y++)
+    {
+        for (x = x0; x <= x1; x++)
+        {
+            vram[y * xsize + x] = c; /*色号给到指定vram处*/
+        }
+    }
+    return;
+}
+
+/*初始化调色板*/
+void init_palette(void)
+{
+    static unsigned char table_rgb[16 * 3] = {
+        0x00, 0x00, 0x00, /**/
+        0xff, 0x00, 0x00, /**/
+        0x00, 0xff, 0x00, /**/
+        0xff, 0xff, 0x00, /**/
+        0x00, 0x00, 0xff, /**/
+        0xff, 0x00, 0xff, /**/
+        0x00, 0xff, 0xff, /**/
+        0xff, 0xff, 0xff, /**/
+        0xc6, 0xc6, 0xc6, /**/
+        0x84, 0x00, 0x00, /**/
+        0x00, 0x84, 0x00, /**/
+        0x84, 0x84, 0x00, /**/
+        0x00, 0x00, 0x84, /**/
+        0x84, 0x00, 0x84, /**/
+        0x00, 0x84, 0x84, /**/
+        0x84, 0x84, 0x84, /**/
+    };
+    set_palette(0, 15, table_rgb);
+    return;
+}
+void set_palette(int start, int end, unsigned char *rgb)
+{
+    int i, eflags;
+    /*设定调色板前首先中断,因此先读入中断标志*/
+    eflags = io_load_eflags(); /*记录中断许可标志*/
+    io_cli();                  /*中断许可标志为0,禁止中断(其实就是执行中断)*/
+    io_out8(0x03c8, start);
+    for (i = start; i <= end; i++)
+    {
+        io_out8(0x03c9, rgb[0] / 4);
+        io_out8(0x03c9, rgb[1] / 4);
+        io_out8(0x03c9, rgb[2] / 4);
+        rgb += 3;
+    }
+    io_store_eflags(eflags); /*恢复中断标志*/
+    return;
+}
