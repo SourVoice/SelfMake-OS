14a37c12c1a10e34aac0b195dd05e47ea37ab349
SourVoice <2985122798@qq.com>
2021-07-17 00:55:37 +0800

    更快的读取与读入中断直接看d

---
+++ day07/c_FIFO/bootpack.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "bootpack.h"
+
+extern struct KEYBUF keybuf; /*表示定义来自外部(其他源文件)(编译太快这里会漏掉编译导致不能通过,可以小改动makefile)*/
+
+void HariMain(void)
+{
+    char s[40];
+    char mouse[256]; /*256字节的指针*/
+
+    char *vram;
+    int xsize, ysize;
+    struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
+    xsize = binfo->scrnx;
+    ysize = binfo->scrny;
+    vram = binfo->vram;
+    unsigned char i;
+    int j;
+
+    init_gdtidt();
+    init_pic();
+    io_sti(); /*中断IF设为1*/
+
+    init_palette();
+    init_screen(vram, xsize, ysize);
+
+    /*鼠标*/
+    init_mouse_cursor8(mouse, COL8_000000);
+    putblock8_8(vram, xsize, 16, 16, xsize / 2, ysize / 2, mouse, 16);
+
+    /*变量显示*/
+    sprintf(s, "scrnx = %d,scrny= %d", xsize, ysize);   /*新添内容,调用sprintf*/
+    putfonts8_asc(vram, xsize, COL8_00ffff, 16, 64, s); /*s用于记录字符串的地址*/
+
+    io_out8(PIC0_IMR, 0xf9);
+    io_out8(PIC1_IMR, 0xef);
+
+    for (;;)
+    {
+        io_cli();             /*屏蔽中断(一次只执行一次中断处理)*/
+        if (keybuf.next == 0) /*检查缓冲区,为空直接进入停机*/
+        {
+            io_stihlt();
+        }
+        else
+        {
+            i = keybuf.data[0];
+            keybuf.next--; /*该中断被处理*/
+            for (j = 0; j < keybuf.next; j++)
+            {
+                keybuf.data[j] = keybuf.data[j + 1];
+            } /*中断处理完毕后其他中断前移*/
+            io_sti();
+            sprintf(s, "%02x", i);
+            boxfill8(vram, xsize, COL8_008484, 0, 16, 15, 31);
+            putfonts8_asc(vram, xsize, COL8_ffffff, 0, 16, s);
+        }
+    }
+}
