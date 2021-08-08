a4eb4399012a0642281621ca46e04b5a80770ca1
SourVoice <2985122798@qq.com>
2021-08-09 03:21:47 +0800

    准备写counter

---
+++ day11/e_counter/graphic.c
@@ -0,0 +1,169 @@
+//屏幕绘制相关
+#include "bootpack.h"
+
+/*初始化调色板*/
+void init_palette(void)
+{
+    static unsigned char table_rgb[16 * 3] = {
+        0x00, 0x00, 0x00, /**/
+        0xff, 0x00, 0x00, /**/
+        0x00, 0xff, 0x00, /**/
+        0xff, 0xff, 0x00, /**/
+        0x00, 0x00, 0xff, /**/
+        0xff, 0x00, 0xff, /**/
+        0x00, 0xff, 0xff, /**/
+        0xff, 0xff, 0xff, /**/
+        0xc6, 0xc6, 0xc6, /**/
+        0x84, 0x00, 0x00, /**/
+        0x00, 0x84, 0x00, /**/
+        0x84, 0x84, 0x00, /**/
+        0x00, 0x00, 0x84, /**/
+        0x84, 0x00, 0x84, /**/
+        0x00, 0x84, 0x84, /**/
+        0x84, 0x84, 0x84, /**/
+    };
+    set_palette(0, 15, table_rgb);
+    return;
+}
+void set_palette(int start, int end, unsigned char *rgb)
+{
+    int i, eflags;
+    eflags = io_load_eflags();
+    io_cli();
+    io_out8(0x03c8, start);
+    for (i = start; i <= end; i++)
+    {
+        io_out8(0x03c9, rgb[0] / 4);
+        io_out8(0x03c9, rgb[1] / 4);
+        io_out8(0x03c9, rgb[2] / 4);
+        rgb += 3;
+    }
+    io_store_eflags(eflags);
+    return;
+}
+void init_screen(unsigned char *vram, int xsize, int ysize)
+{
+    boxfill8(vram, xsize, COL8_008484, 0, 0, xsize - 1, ysize - 29); /*背景色*/
+
+    boxfill8(vram, xsize, COL8_c6c6c6, 0, ysize - 28, xsize - 1, ysize - 28);
+    boxfill8(vram, xsize, COL8_ffffff, 0, ysize - 27, xsize - 1, ysize - 27);
+    boxfill8(vram, xsize, COL8_c6c6c6, 0, ysize - 26, xsize - 1, ysize - 1);
+
+    boxfill8(vram, xsize, COL8_ffffff, 3, ysize - 24, 59, ysize - 24);
+    boxfill8(vram, xsize, COL8_ffffff, 2, ysize - 24, 2, ysize - 4);
+    boxfill8(vram, xsize, COL8_848484, 3, ysize - 4, 59, ysize - 4);
+    boxfill8(vram, xsize, COL8_848484, 59, ysize - 23, 59, ysize - 5);
+    boxfill8(vram, xsize, COL8_000000, 2, ysize - 3, 59, ysize - 3);
+    boxfill8(vram, xsize, COL8_000000, 60, ysize - 24, 60, ysize - 3);
+
+    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize - 4, ysize - 24);
+    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize - 3);
+    boxfill8(vram, xsize, COL8_ffffff, xsize - 47, ysize - 3, xsize - 4, ysize - 3);
+    boxfill8(vram, xsize, COL8_ffffff, xsize - 3, ysize - 24, xsize - 3, ysize - 3);
+}
+
+/*显示字符串*/
+void putfonts8_asc(unsigned char *vram, int xsize, unsigned char color, int x, int y, unsigned char *str)
+{
+    extern char hankaku[4096];
+    for (; *str != 0x00; str++)
+    {
+        putfont8(vram, xsize, color, x, y, hankaku + (*str) * 16);
+        x += 8;
+    }
+    return;
+}
+/*显示字体*/
+void putfont8(char *vram, int xsize, char color, int x, int y, char *font)
+{
+    int i;
+    char d;
+    char *p;
+    for (i = 0; i < 16; i++) /*16行*/
+    {
+        p = vram + (y + i) * xsize + x;
+        d = font[i];
+        if ((d & 0x80) != 0)
+            *(p + 0) = color;
+        if ((d & 0x40) != 0)
+            *(p + 1) = color;
+        if ((d & 0x20) != 0)
+            *(p + 2) = color;
+        if ((d & 0x10) != 0)
+            *(p + 3) = color;
+        if ((d & 0x08) != 0)
+            *(p + 4) = color;
+        if ((d & 0x04) != 0)
+            *(p + 5) = color;
+        if ((d & 0x02) != 0)
+            *(p + 6) = color;
+        if ((d & 0x01) != 0)
+            *(p + 7) = color;
+    }
+    return;
+}
+/*初始化屏幕,填充颜色,0表示左上角,1表示右上角*/
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
+{
+    int x, y;
+    for (y = y0; y <= y1; y++)
+    {
+        for (x = x0; x <= x1; x++)
+        {
+            vram[y * xsize + x] = c; /*色号给到指定vram处*/
+        }
+    }
+    return;
+}
+/*绘制鼠标*/
+void init_mouse_cursor8(char *mouse, char bc_color)
+{
+    static char cursor[16][16] = {"**************..",
+                                  "*OOOOOOOOOOO*...",
+                                  "*OOOOOOOOOO*....",
+                                  "*OOOOOOOOO*.....",
+                                  "*OOOOOOOO*......",
+                                  "*OOOOOOO*.......",
+                                  "*OOOOOOO* ......",
+                                  "*OOOOOOOO*......",
+                                  "*OOOO**OOO*.....",
+                                  "*OOO*..*OOO*....",
+                                  "*OO*....*OOO*...",
+                                  "*O*......*OOO*..",
+                                  "**........*OOO*.",
+                                  "*..........*OOO*",
+                                  "............*OO*",
+                                  ".............***"};
+    int x, y;
+    for (y = 0; y < 16; y++)
+    {
+        for (x = 0; x < 16; x++)
+        {
+            if (cursor[y][x] == '*')
+            {
+                *(mouse + y * 16 + x) = COL8_000084;
+            }
+            if (cursor[y][x] == 'O')
+            {
+                *(mouse + y * 16 + x) = COL8_00ffff;
+            }
+            if (cursor[y][x] == '.')
+            {
+                *(mouse + y * 16 + x) = bc_color;
+            }
+        }
+    }
+    return;
+}
+void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize)
+{
+    int x, y;
+    for (y = 0; y < pysize; y++) /*这里的坐标从0开始,到pysize结束,是屏幕的 ys*/
+    {
+        for (x = 0; x < pxsize; x++)
+        {
+            *(vram + (y + py0) * vxsize + (x + px0)) = *(buf + y * bxsize + x);
+        }
+    }
+    return;
+}
---
+++ day11/e_counter/sheet.c
@@ -0,0 +1,213 @@
+#include "bootpack.h"
+
+struct SHTCTL *shtctl_init(struct MEMMAN *memman, unsigned char *vram, int xsize, int ysize)
+{
+    struct SHTCTL *ctl;
+    int i;
+    ctl = (struct SHTCTL *)memman_alloc_4k(memman, sizeof(struct SHTCTL));
+    if (ctl == 0)
+    {
+        goto err;
+    }
+    ctl->vram = vram; /*binfo内容,避免你每次都从内存读取*/
+    ctl->xsize = xsize;
+    ctl->ysize = ysize;
+    ctl->top = -1;
+    for (i = 0; i < MAX_SHEETS; i++)
+    {
+        ctl->sheets0[i].flags = 0; /*图层未使用*/
+        ctl->sheets0[i].ctl = ctl; /*每个sheet都赋一个记录表*/
+    }
+err:
+    return ctl;
+}
+/*返回一个新分配的图层*/
+struct SHEET *sheet_alloc(struct SHTCTL *ctl)
+{
+    struct SHEET *sht;
+    int i;
+    for (i = 0; i < MAX_SHEETS; i++)
+    {
+        if (ctl->sheets0[i].flags == 0) /*该图层未使用*/
+        {
+            sht = &ctl->sheets0[i];
+            sht->flags = SHEET_USE; /*正在使用*/
+            sht->height = -1;       /*隐藏*/
+            return sht;
+        }
+    }
+    return 0;
+}
+void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, int color_inv)
+{
+    sht->buf = buf;
+    sht->bxsize = xsize;
+    sht->bysize = ysize;
+    sht->color_inv = color_inv;
+    return;
+}
+/*设定底板高度*/
+void sheet_updown(struct SHEET *sht, int height)
+{
+    struct SHTCTL *ctl = sht->ctl;
+    int h, old = sht->height; /*存储设置前的高度信息*/
+    /*指定高度强制介于top~-1之间*/
+    if (height > ctl->top + 1)
+    {
+        height = ctl->top + 1;
+    }
+    if (height < -1)
+    {
+        height = -1;
+    }
+    sht->height = height; /*设定图层高度*/
+
+    if (old > height) /*图层height小于以前*/
+    {
+        if (height >= 0)
+        {
+            for (h = old; h > height; h--) /*介于old到height之间的图层向上移*/
+            {
+                ctl->sheets[h] = ctl->sheets[h - 1];
+                ctl->sheets[h]->height = h;
+            }
+            ctl->sheets[height] = sht; /*图层全部移好后将sht放到设定的height处*/
+        }
+        else /*height = -1 (隐藏)*/
+        {
+            if (ctl->top > old)
+            {
+                for (h = old; h < ctl->top; h++)
+                {
+                    ctl->sheets[h] = ctl->sheets[h + 1];
+                    ctl->sheets[h]->height = h;
+                }
+            }
+            ctl->top--; /*显示图层减少,最上面图层高度下降*/
+        }
+        sheet_refreshsub(ctl, sht->vram_x0, sht->vram_y0, sht->vram_x0 + sht->bxsize, sht->vram_y0 + sht->bysize); /*显示重新排布好的图层*/
+    }
+    else if (old < height) /*设定位置比以前高*/
+    {
+        if (old >= 0)
+        {
+            for (h = old; h < height; h++)
+            {
+                /*图层下移*/
+                ctl->sheets[h] = ctl->sheets[h + 1];
+                ctl->sheets[h]->height = h;
+            }
+            ctl->sheets[height] = sht;
+        }
+        else /*old = -1 (原来隐藏状态)*/
+        {
+            for (h = ctl->top; h >= height; h--)
+            {
+                ctl->sheets[h + 1] = ctl->sheets[h];
+                ctl->sheets[h + 1]->height = h + 1;
+            }
+            ctl->sheets[height] = sht;
+            ctl->top++;
+        }
+        sheet_refreshsub(ctl, sht->vram_x0, sht->vram_y0, sht->vram_x0 + sht->bxsize, sht->vram_y0 + sht->bysize); /*显示重新排布好的图层*/
+    }
+    return;
+}
+/*从上到下描绘图层*/
+void sheet_refresh(struct SHEET *sht, int bx0, int by0, int bx1, int by1)
+{
+    if (sht->height >= 0)
+    {
+        sheet_refreshsub(sht->ctl, sht->vram_y0 + bx0, sht->vram_y0 + by0, sht->vram_x0 + bx1, sht->vram_y0 + by1);
+    }
+    return;
+}
+void sheet_refreshsub(struct SHTCTL *ctl, int vram_x0, int vram_y0, int vram_x1, int vram_y1)
+{
+    int h, bx, by, vx, vy, bx0, by0, bx1, by1; /*bx0,by0,bx1,by1表示将要调整到的位置*/
+    unsigned char *buf, c, *vram = ctl->vram;
+    struct SHEET *sht;
+    /*refresh超出范围进行画面修正*/
+    if (vram_x0 < 0)
+    {
+        vram_x0 = 0;
+    }
+    if (vram_y0 < 0)
+    {
+        vram_y0 = 0;
+    }
+    if (vram_x1 > ctl->xsize)
+    {
+        vram_x1 = ctl->xsize;
+    }
+    if (vram_y1 > ctl->ysize)
+    {
+        vram_y1 = ctl->ysize;
+    }
+    for (h = 0; h <= ctl->top; h++)
+    {
+        sht = ctl->sheets[h];
+        buf = sht->buf;
+        /*从vx0~vy1，对bx0~by1进行倒推*/
+        bx0 = vram_x0 - sht->vram_x0;
+        by0 = vram_y0 - sht->vram_y0;
+        bx1 = vram_x1 - sht->vram_x0;
+        by1 = vram_y1 - sht->vram_y0;
+        if (bx0 < 0)
+        {
+            bx0 = 0;
+        }
+        if (by0 < 0)
+        {
+            by0 = 0;
+        }
+        if (bx1 > sht->bxsize)
+        {
+            bx1 = sht->bxsize;
+        }
+        if (by1 > sht->bysize)
+        {
+            by1 = sht->bysize;
+        }
+        for (by = by0; by < by1; by++)
+        {
+            vy = sht->vram_y0 + by;
+            for (bx = 0; bx < sht->bxsize; bx++)
+            {
+                vx = sht->vram_x0 + bx;
+                if (vram_x0 <= vx && vx < vram_x1 && vram_y0 <= vy && vy < vram_y1)
+                {
+                    c = buf[by * sht->bxsize + bx];
+                    if (c != sht->bxsize + vx)
+                    {
+                        vram[vy * ctl->xsize + vx] = c;
+                    }
+                }
+            }
+        }
+    }
+    return;
+}
+/*上下左右移动图层*/
+void sheet_slide(struct SHEET *sht, int vram_x0, int vram_y0)
+{
+    int old_vx0 = sht->vram_x0, old_vy0 = sht->vram_y0; /*记住移动之前的位置*/
+    sht->vram_x0 = vram_x0;
+    sht->vram_y0 = vram_y0;
+    if (sht->height >= 0)
+    {
+        sheet_refreshsub(sht->ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize);
+        sheet_refreshsub(sht->ctl, vram_x0, vram_y0, vram_x0 + sht->bxsize, vram_y0 + sht->bysize);
+    }
+    return;
+}
+
+void sheet_free(struct SHEET *sht)
+{
+    if (sht->height >= 0)
+    {
+        sheet_updown(sht, -1);
+    }
+    sht->flags = 0;
+    return;
+}
