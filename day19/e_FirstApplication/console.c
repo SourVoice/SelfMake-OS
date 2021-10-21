9bdd4fc8fde2a0376ced4971f211e91f4a1df148
SourVoice <2985122798@qq.com>
2021-10-21 21:16:57 +0800

    添加第一个应用程序

---
+++ day19/e_FirstApplication/console.c
@@ -0,0 +1,306 @@
+#include "bootpack.h"
+#include <string.h>
+
+void mem_command(struct MEMMAN *memman, unsigned char memtotal, char *s, int *cursor_y, struct SHEET *sheet)
+{
+    sprintf(s, "total   %dMB", memtotal / (1024 * 1024));
+    putfonts8_asc_sht(sheet, 8, *cursor_y, COL8_FFFFFF, COL8_000000, s, 30);
+    *cursor_y = cons_newline(*cursor_y, sheet);
+    sprintf(s, "free %dKB", memman_total(memman) / 1024);
+    putfonts8_asc_sht(sheet, 8, *cursor_y, COL8_FFFFFF, COL8_000000, s, 30);
+    *cursor_y = cons_newline(*cursor_y, sheet);
+    *cursor_y = cons_newline(*cursor_y, sheet);
+}
+
+void cls_command(struct SHEET *sheet, int *cursor_y, int x, int y)
+{
+    for (y = 28; y < 28 + 128; y++)
+    {
+        for (x = 8; x < 8 + 240; x++)
+        {
+            sheet->buf[x + y * sheet->bxsize] = COL8_000000;
+        }
+    }
+    sheet_refresh(sheet, 8, 28, 8 + 240, 28 + 128);
+    *cursor_y = 28;
+}
+
+void dir_command(struct FILEINFO *finfo, struct SHEET *sheet, int x, int y, int *cursor_y, char *s)
+{
+    for (x = 0; x < 224; x++) /*最多224个文件信息*/
+    {
+        if (finfo[x].name[0] == 0x00) /*文件名第一个字节为0x00表示不包含任何信息*/
+        {
+            break;
+        }
+        if (finfo[x].name[0] != 0xe5) /*文件名第一个字节未0xe5表示已被删除*/
+        {
+            if ((finfo[x].type & 0x18) == 0)
+            {
+                sprintf(s, "filename.ext %7d", finfo[x].size);
+                for (y = 0; y < 8; y++)
+                {
+                    s[y] = finfo[x].name[y]; /*文件名*/
+                }
+                s[9] = finfo[x].ext[0];
+                s[10] = finfo[x].ext[1];
+                s[11] = finfo[x].ext[2];
+                putfonts8_asc_sht(sheet, 8, *cursor_y, COL8_FFFFFF, COL8_000000, s, 30);
+                *cursor_y = cons_newline(*cursor_y, sheet);
+            }
+        }
+    }
+    *cursor_y = cons_newline(*cursor_y, sheet);
+}
+void type_command()
+{
+}
+
+void console_task(struct SHEET *sheet, unsigned int memtotal)
+{
+    struct TIMER *timer;
+    struct TASK *task = task_now();
+    int i, fifobuf[128], cursor_x = 16, cursor_y = 28, cursor_c = -1;
+    char s[30], cmdline[30], *p;
+    struct MEMMAN *memman = (struct MEMMAN *)MEMMAN_ADDR;
+    int x = 0, y = 0;
+    struct FILEINFO *finfo = (struct FILEINFO *)(ADR_DISKIMG + 0x002600);
+    int *fat = (int *)memman_alloc_4k(memman, 4 * 2800);
+
+    file_readfat(fat, (unsigned char *)(ADR_DISKIMG + 0x000200));
+    fifo32_init(&task->fifo, 128, fifobuf, task);
+    timer = timer_alloc();
+    timer_init(timer, &task->fifo, 1);
+    timer_settime(timer, 50);
+
+    /*显示提示符*/
+    putfonts8_asc_sht(sheet, 8, 28, COL8_FFFFFF, COL8_000000, ">", 1);
+
+    for (;;)
+    {
+        io_cli();
+        if (fifo32_status(&task->fifo) == 0)
+        {
+            task_sleep(task);
+            io_sti();
+        }
+        else
+        {
+            i = fifo32_get(&task->fifo);
+            io_sti();
+            if (i <= 1) /*光标用定时器*/
+            {
+                if (i != 0)
+                {
+                    timer_init(timer, &task->fifo, 0); /*下次置0 */
+                    if (cursor_c >= 0)
+                    {
+                        cursor_c = COL8_FFFFFF;
+                    }
+                }
+                else
+                {
+                    timer_init(timer, &task->fifo, 1); /*下次置1 */
+                    if (cursor_c >= 0)
+                    {
+                        cursor_c = COL8_000000;
+                    }
+                }
+                timer_settime(timer, 50);
+            }
+            if (i == 2)
+            { /*光标ON */
+                cursor_c = COL8_FFFFFF;
+            }
+            if (i == 3)
+            { /*光标OFF */
+                boxfill8(sheet->buf, sheet->bxsize, COL8_000000, cursor_x, cursor_y, cursor_x + 7, cursor_y + 15);
+                cursor_c = -1;
+            }
+            if (256 <= i && i <= 511) /*键盘数据（通过任务A） */
+            {
+                if (i == 8 + 256) /*退格键*/
+                {
+                    if (cursor_x > 16)
+                    {
+                        /*用空白擦除光标后将光标前移一位*/
+                        putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, " ", 1);
+                        cursor_x -= 8;
+                    }
+                }
+                else if (i == 10 + 256) /*回车键*/
+                {
+                    /*用空格将光标擦除*/
+                    putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, " ", 1);
+                    cmdline[cursor_x / 8 - 2] = 0;
+                    cursor_y = cons_newline(cursor_y, sheet);
+                    /*执行命令*/
+                    if (strcmp(cmdline, "mem") == 0) /* mem命令*/
+                    {
+                        mem_command(memman, memtotal, s, &cursor_y, sheet);
+                    }
+                    else if (strcmp(cmdline, "cls") == 0) /* cls命令*/
+                    {
+                        cls_command(sheet, &cursor_y, x, y);
+                    }
+                    else if (strcmp(cmdline, "dir") == 0 || strcmp(cmdline, "ls") == 0) /* dir命令 */
+                    {
+                        dir_command(finfo, sheet, x, y, &cursor_y, s);
+                    }
+                    else if (strncmp(cmdline, "type", 5)) /*type命令(仅比较前五个字符)*/
+                    {
+                        for (y = 0; y < 11; y++)
+                        {
+                            s[y] = ' ';
+                        }
+                        y = 0;
+                        for (x = 5; y < 11 && cmdline[x] != 0; x++)
+                        {
+                            if (cmdline[x] == '.' && y <= 8)
+                            {
+                                y = 8;
+                            }
+                            else
+                            {
+                                s[y] = cmdline[x];
+                                if ('a' <= s[y] && s[y] <= 'z') /*小写改为大写*/
+                                {
+                                    /*将小写字母转换成大写字母 */
+                                    s[y] -= 0x20;
+                                }
+                                y++;
+                            }
+                        }
+                        for (x = 0; x < 224;) /*搜索文件*/
+                        {
+                            if (finfo[x].name[0] == 0x00)
+                            {
+                                break;
+                            }
+                            if ((finfo[x].type & 0x18) == 0)
+                            {
+                                for (y = 0; y < 11; y++)
+                                {
+                                    if (finfo[x].name[y] != s[y])
+                                    {
+                                        goto type_next_file;
+                                    }
+                                }
+                                break; /*找到文件*/
+                            }
+                        type_next_file:
+                            x++;
+                        }
+                        if (x < 224 && finfo[x].name[0] != 0x00) /*找到文件的情况*/
+                        {
+                            p = (char *)memman_alloc_4k(memman, finfo[x].size);
+                            file_loadfile(finfo[x].clustno, finfo[x].size, p, fat, (char *)(ADR_DISKIMG + 0x003e00)); /*解压缩写入内存*/
+                            cursor_x = 8;
+                            for (y = 0; y < finfo[x].size; y++)
+                            {
+                                s[0] = p[y];
+                                s[1] = 0;
+                                if (s[0] == 0x09) /*制表符*/
+                                {
+                                    for (;;)
+                                    {
+                                        putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, "", 1);
+                                        cursor_x += 8;
+                                        if (cursor_x == 8 + 240)
+                                        {
+                                            cursor_x = 8;
+                                            cursor_y = cons_newline(cursor_y, sheet);
+                                        }
+                                        if (((cursor_x - 8) & 0x1f) == 0) /*被32整除*/
+                                        {
+                                            break;
+                                        }
+                                    }
+                                }
+                                else if (s[0] == 0x0a) /*换行*/
+                                {
+                                    cursor_x = 8;
+                                    cursor_y = cons_newline(cursor_y, sheet);
+                                }
+                                else if (s[0] == 0x0d) /*回车*/
+                                {
+                                }
+                                else /*一般字符*/
+                                {
+                                    putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, s, 1);
+                                    cursor_x += 8;
+                                    if (cursor_x == 8 + 240)
+                                    {
+                                        cursor_x = 8;
+                                        cursor_y = cons_newline(cursor_y, sheet);
+                                    }
+                                }
+                            }
+                            memman_free_4k(memman, (int)p, finfo[x].size);
+                        }
+                        else /*没有找到文件*/
+                        {
+                            putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, "not find", 8);
+                            cursor_y = cons_newline(cursor_y, sheet);
+                        }
+                        cursor_y = cons_newline(cursor_y, sheet);
+                    }
+                    else if (cmdline[0] != 0) /*不是命令，也不是空行 */
+                    {
+                        putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, "Bad command.", 12);
+                        cursor_y = cons_newline(cursor_y, sheet);
+                        cursor_y = cons_newline(cursor_y, sheet);
+                    }
+                    /*显示提示符*/
+                    putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, ">", 1);
+                    cursor_x = 16;
+                }
+                else /*一般字符*/
+                {
+                    if (cursor_x < 240)
+                    {
+                        /*显示一个字符之后将光标后移一位 */
+                        s[0] = i - 256;
+                        s[1] = 0;
+                        cmdline[cursor_x / 8 - 2] = i - 256; /*输入内容存入cmdline*/
+                        putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, s, 1);
+                        cursor_x += 8;
+                    }
+                }
+            }
+            /*重新显示光标*/
+            if (cursor_c >= 0)
+            {
+                boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x, cursor_y, cursor_x + 7, cursor_y + 15);
+            }
+            sheet_refresh(sheet, cursor_x, cursor_y, cursor_x + 8, cursor_y + 16);
+        }
+    }
+}
+int cons_newline(int cursor_y, struct SHEET *sheet)
+{
+    int x, y;
+    if (cursor_y < 28 + 112) /*换行*/
+    {
+        cursor_y += 16;
+    }
+    else /*滚动*/
+    {
+        for (y = 28; y < 28 + 112; y++) /*下一行给上一行*/
+        {
+            for (x = 8; x < 8 + 240; x++)
+            {
+                sheet->buf[x + y * sheet->bxsize] = sheet->buf[x + (y + 16) * sheet->bxsize];
+            }
+        }
+        for (y = 28 + 112; y < 28 + 128; y++)
+        {
+            for (x = 8; x < 8 + 240; x++)
+            {
+                sheet->buf[x + y * sheet->bxsize] = COL8_000000;
+            }
+        }
+        sheet_refresh(sheet, 8, 28, 8 + 240, 28 + 128);
+    }
+    return cursor_y;
+}
